9ea05be72e781075b6ca5cf35d7e94cc0bdaea02
Shravan Bobade <shravanbobade@Shravans-Laptop.local>
2024-08-08 22:29:08 +0530

    added middleware and error

--- api/index.js
+++ api/index.js
@@ -18,3 +18,13 @@ app.listen(port, () => {
 
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
