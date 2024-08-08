f573e912806756c4809f655a47f957f6f17de033
Shravan Bobade <shravanbobade@Shravans-Laptop.local>
2024-08-08 20:03:23 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
