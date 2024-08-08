546a7ca7fd898a5445618ef12796b7c702eaf822
Shravan Bobade <shravanbobade@Shravans-Laptop.local>
2024-08-08 21:40:02 +0530

    tested api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -12,3 +13,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.use("/api/user", userRoute);
