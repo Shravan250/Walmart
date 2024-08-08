6342555528d8098185beaf559efc39530ef88e28
Shravan Bobade <shravanbobade@Shravans-Laptop.local>
2024-08-09 00:34:49 +0530

    created signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { errorhandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -9,6 +10,34 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "User registered successfully!" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return next(errorhandler(404, "User not found"));
+  }
+  const isMatch = bcrypt.compareSync(password, user.password);
+  if (!isMatch) {
+    return next(errorhandler(401, "Invalid credentials"));
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+  const { password: hashedPassword, ...rest } = user._doc;
+
+  const expiryDate = new Date();
+  expiryDate.setDate(expiryDate.getDate() + 7);
+
+  res
+    .cookie("access_token", token, { httpsOnly: true, expires: expiryDate })
+    .status(200)
+    .json(rest);
+  try {
   } catch (err) {
     next(err);
   }
